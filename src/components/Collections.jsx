d558c0d5c1be112ddc29089e71052257df3ca8c5
Savindu Dhamsara <savindudhamsara2001@gmail.com>
2022-05-11 22:32:00 +0530

    Header section changed

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -2,23 +2,23 @@ import React from "react"
 
 const callouts = [
   {
-    name: 'Desk and Office',
-    description: 'Work from home accessories',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+    name: 'France',
+    description: 'France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion houses,',
+    imageSrc: 'https://images.unsplash.com/photo-1549144511-f099e773c147?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJhbmNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
     imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
     href: '#',
   },
   {
-    name: 'Self-Improvement',
-    description: 'Journals and note-taking',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+    name: 'South Korea',
+    description: 'South Korea, an East Asian nation on the southern half of the Korean Peninsula, shares one of the world’s most heavily militarized borders with North Korea.',
+    imageSrc: 'https://images.unsplash.com/photo-1583833008338-31a6657917ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c291dGglMjBrb3JlYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
     imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
     href: '#',
   },
   {
-    name: 'Travel',
-    description: 'Daily commute essentials',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+    name: 'United Kingdom',
+    description: 'The United Kingdom, made up of England, Scotland, Wales and Northern Ireland, is an island nation in northwestern Europe. England – birthplace of Shakespeare and The Beatles – is home to the capital, London, a globally influential centre of finance and culture.',
+    imageSrc: 'https://images.unsplash.com/photo-1467681336765-a6d1c58c61e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dW5pdGVkJTIwa2luZG9tfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
     imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
     href: '#',
   },
@@ -26,10 +26,10 @@ const callouts = [
 
 export default function Example() {
   return (
-    <div className="bg-amber-100">
+    <div className="bg-orange-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
-          <h2 className="text-2xl font-extrabold text-gray-900">Collections</h2>
+          <h2 className="text-4xl font-extrabold text-amber-500">Flight Collection</h2>
 
           <div className="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
             {callouts.map((callout) => (
@@ -41,9 +41,9 @@ export default function Example() {
                     className="w-full h-full object-center object-cover"
                   />
                 </div>
-                <h3 className="mt-6 text-sm text-gray-500">
+                <h3 className="mt-6 text-3xl text-amber-700">
                   <a href={callout.href}>
-                    <span className="absolute inset-0" />
+                    <span className="absolute inset-0 " />
                     {callout.name}
                   </a>
                 </h3>
--- src/components/Flights.jsx
+++ src/components/Flights.jsx
@@ -73,7 +73,7 @@ export default function Example() {
             {product.breadcrumbs.map((breadcrumb) => (
               <li key={breadcrumb.id}>
                 <div className="flex items-center">
-                  <a href={breadcrumb.href} className="mr-2 text-sm font-medium text-gray-900">
+                  <a href={breadcrumb.href} className="mr-2 text-lg font-medium text-gray-900">
                     {breadcrumb.name}
                   </a>
                   <svg
@@ -90,7 +90,7 @@ export default function Example() {
                 </div>
               </li>
             ))}
-            <li className="text-sm">
+            <li className="text-2xl">
               <a href={product.href} aria-current="page" className="font-medium text-gray-500 hover:text-gray-600">
                 {product.name}
               </a>
@@ -135,7 +135,7 @@ export default function Example() {
         {/* Product info */}
         <div className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
           <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-            <h1 className="text-2xl font-extrabold tracking-tight text-amber-500 sm:text-3xl">{product.name}</h1>
+            <h1 className="text-4xl font-extrabold tracking-tight text-amber-500 sm:text-3xl">{product.name}</h1>
           </div>
 
           {/* Options */}
