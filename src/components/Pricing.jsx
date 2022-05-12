db555079f666e7566df794425ad20e54e797f44c
Savindu Dhamsara <savindudhamsara2001@gmail.com>
2022-05-12 12:01:29 +0530

    Pricing section changed

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { CheckIcon } from '@heroicons/react/solid';
 
+
 const Pricing = () => {
   return (
     <div name='pricing' className='w-full text-white my-24'>
@@ -21,69 +22,61 @@ const Pricing = () => {
 
         <div className='grid md:grid-cols-2'>
 
-          <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
-            <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Standard (Economy Class) </span>
+          <div className='bg-white text-slate-900 m-8 p-10 rounded-xl shadow-2xl relative'>
+            <span className='uppercase px-3 py-1 bg-amber-200 text-indigo-900 rounded-2xl text-lg'>Standard (Economy Class) </span>
             <div>
-              <p className='text-6xl font-bold py-4 flex'>$490<span className='text-xl text-slate-500 flex flex-col justify-end'>/One Way</span></p>
+              <p className='text-6xl font-bold py-4 flex'>$990<span className='text-xl text-slate-500 flex flex-col justify-end'>/One Way</span></p>
             </div>
-            <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            <p className='text-3xl py-8 text-slate-500'> From Sri Lanka to Switzerland</p>
             <div className='text-2xl'>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Cheaper fares</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Free foods and snacks</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Inflight entertainment</p>
                 <button className='w-full py-4 my-4'>Start Book</button>
             </div>
           </div>
-          <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
-            <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Premium (Business Class)</span>
+          <div className='bg-white text-slate-900 m-8 p-10 rounded-xl shadow-2xl relative'>
+            <span className='uppercase px-3 py-1 bg-amber-200 text-indigo-900 rounded-2xl text-lg'>Premium (Business Class)</span>
             <div>
-              <p className='text-6xl font-bold py-4 flex'>$999<span className='text-xl text-slate-500 flex flex-col justify-end'>/One Way</span></p>
+              <p className='text-6xl font-bold py-4 flex'>$1500<span className='text-xl text-slate-500 flex flex-col justify-end'>/One Way</span></p>
             </div>
-            <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            <p className='text-3xl py-8 text-slate-500'>From Sri Lanka to Switzerland</p>
             <div className='text-2xl'>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Prority boarding</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Amenities & Facilities </p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Inflight entertainment</p>
                 <button className='w-full py-4 my-4'>Start Book</button>
             </div>
           </div>
         </div>
          <div className='grid md:grid-cols-2'>
-
-          <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
-            <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Standard</span>
+         <div className='bg-white text-slate-900 m-8 p-10 rounded-xl shadow-2xl relative'>
+            <span className='uppercase px-3 py-1 bg-amber-200 text-indigo-900 rounded-2xl text-lg'>Standard (Economy Class) </span>
             <div>
-              <p className='text-6xl font-bold py-4 flex'>$49<span className='text-xl text-slate-500 flex flex-col justify-end'>/mo</span></p>
+              <p className='text-6xl font-bold py-4 flex'>$980<span className='text-xl text-slate-500 flex flex-col justify-end'>/One Way</span></p>
             </div>
-            <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            <p className='text-3xl py-8 text-slate-500'> From Sri Lanka to Japan</p>
             <div className='text-2xl'>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <button className='w-full py-4 my-4'>Get Started</button>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Cheaper fares</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Free foods and snacks</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Inflight entertainment</p>
+                <button className='w-full py-4 my-4'>Start Book</button>
             </div>
           </div>
-          <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
-            <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Premium</span>
+          <div className='bg-white text-slate-900 m-8 p-10 rounded-xl shadow-2xl relative'>
+            <span className='uppercase px-3 py-1 bg-amber-200 text-indigo-900 rounded-2xl text-lg'>Premium (Business Class)</span>
             <div>
-              <p className='text-6xl font-bold py-4 flex'>$99<span className='text-xl text-slate-500 flex flex-col justify-end'>/mo</span></p>
+              <p className='text-6xl font-bold py-4 flex'>$1200<span className='text-xl text-slate-500 flex flex-col justify-end'>/One Way</span></p>
             </div>
-            <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            <p className='text-3xl py-8 text-slate-500'>From Sri Lanka to Japan</p>
             <div className='text-2xl'>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
-                <button className='w-full py-4 my-4'>Get Started</button>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Additional Baggage Allowance</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Fine Dining</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Inflight entertainment</p>
+                <button className='w-full py-4 my-4'>Start Book</button>
             </div>
           </div>
+          
         </div>
       </div>
     </div>
